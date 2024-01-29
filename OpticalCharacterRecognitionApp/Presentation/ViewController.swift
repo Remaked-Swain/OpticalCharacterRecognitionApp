a2501afdb2773f15c96a713d8d8eeb62e655c967
YunSwain <destap@naver.com>
2024-01-29 15:38:00 +0900

    feat: Creatio

---
+++ OpticalCharacterRecognitionApp/Presentation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  OpticalCharacterRecognitionApp
+//
+//  Created by Swain Yun on 1/29/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
