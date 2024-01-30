76673be1e8136c282f4a76da636b607ea226fdcd
YunSwain <destap@naver.com>
2024-01-30 23:22:27 +0900

    feat: UnitTest 생성

---
+++ OpticalCharacterRecognitionAppTests/OpticalCharacterRecognitionAppTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+final class OpticalCharacterRecognitionAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+    
+    // Test code here...
+}
