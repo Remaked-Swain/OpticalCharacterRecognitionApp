237481cb93c6adb3d0811b0134e488684c432be5
YunSwain <destap@naver.com>
2024-02-03 20:11:57 +0900

    feat: 명함 인식을 위한 Detector 구현

--- OpticalCharacterRecognitionApp/Domain/Entity/Detector/DefaultDocumentDetector.swift
+++ OpticalCharacterRecognitionApp/Domain/Entity/Detector/DefaultDocumentDetector.swift
@@ -1,43 +1,77 @@
 import Foundation
 import CoreImage
+import Vision
 
-protocol DocumentDetector {
-    func detect(in ciImage: CIImage) throws -> CIRectangleFeature
+protocol VisionTrackerProcessDelegate: AnyObject {
+    func notifyTrackingResult(_ delegate: DefaultDocumentDetector, rectangle: TrackedRectangle?, error: Error?)
+    func didFinishTracking(_ delegate: DefaultDocumentDetector, isFinish: Bool)
 }
 
-final class DefaultDocumentDetector: DocumentDetector {
-    
+final class DefaultDocumentDetector {
     // MARK: Properties
-    private let detector: CIDetector? = CIDetector(ofType: CIDetectorTypeRectangle,
-                                                  context: nil,
-                                                  options: [
-                                                    CIDetectorAccuracy: CIDetectorAccuracyHigh,
-                                                    CIDetectorMinFeatureSize: NSNumber(0.3)
-                                                  ])
-    private let options: [String: Any]? = [CIDetectorAspectRatio: NSNumber(1.414)]
+    private var lastObservation: VNRectangleObservation?
+    private let visionSequenceHandler: VNSequenceRequestHandler = VNSequenceRequestHandler()
+    private var detectionTimer: Timer?
+    
+    // MARK: Dependencies
+    weak var delegate: VisionTrackerProcessDelegate?
     
     // MARK: Interface
-    func detect(in ciImage: CIImage) throws -> CIRectangleFeature {
-        let rectangles = try extractFeature(in: ciImage, options: options)
-        guard let biggestRectangle = findBiggestRectangleFeature(with: rectangles) else {
-            throw DetectError.detectFailed
+    func detect(on pixelBuffer: CVPixelBuffer) throws {
+        guard let lastObservation = lastObservation else {
+            let request = VNDetectRectanglesRequest(completionHandler: handleDetectionRequestUpdate)
+            let handler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer)
+            try handler.perform([request])
+            return
         }
-        return biggestRectangle
+        try detectRectangleOnPixelBuffer(on: pixelBuffer, detectedObservation: lastObservation)
     }
 }
 
 // MARK: Private Methods
 extension DefaultDocumentDetector {
-    private func extractFeature(in ciImage: CIImage, options: [String: Any]?) throws -> [CIRectangleFeature] {
-        guard let rectangles = detector?.features(in: ciImage, options: options) as? [CIRectangleFeature] else {
-            throw DetectError.imageNotFound
-        }
-        return rectangles
+    private func detectRectangleOnPixelBuffer(on pixelBuffer: CVPixelBuffer, detectedObservation: VNRectangleObservation) throws {
+        let request = VNTrackRectangleRequest(rectangleObservation: detectedObservation, completionHandler: handleDetectionRequestUpdate)
+        request.trackingLevel = .accurate
+        
+        try visionSequenceHandler.perform([request], on: pixelBuffer)
     }
     
-    private func findBiggestRectangleFeature(with rectangles: [CIRectangleFeature]) -> CIRectangleFeature? {
-        return rectangles.sorted {
-            $0.area > $1.area
-        }.first
+    private func handleDetectionRequestUpdate(_ request: VNRequest, error: Error?) {
+        guard let newObservation = request.results?.first as? VNRectangleObservation else {
+            print("감지 실패 --------- 1")
+            invalidateDetectionTimer()
+            return
+        }
+        
+        lastObservation = newObservation
+        
+        guard newObservation.confidence >= 0.3 else {
+            print("낮은 신뢰도 --------- 2")
+            delegate?.notifyTrackingResult(self, rectangle: nil, error: DetectError.rectangleDetectionFailed)
+            invalidateDetectionTimer()
+            return
+        }
+        
+        let trackedRectangle = TrackedRectangle(observation: newObservation)
+        print("감지 성공 --------- 3")
+        delegate?.notifyTrackingResult(self, rectangle: trackedRectangle, error: nil)
+        
+        setUpDetectionTimer()
+    }
+    
+    private func setUpDetectionTimer() {
+        guard detectionTimer == nil else { return }
+        
+        detectionTimer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false) { [weak self] _ in
+            guard let self = self else { return }
+            delegate?.didFinishTracking(self, isFinish: true)
+        }
+        detectionTimer?.fire()
+    }
+    
+    private func invalidateDetectionTimer() {
+        detectionTimer?.invalidate()
+        detectionTimer = nil
     }
 }
--- OpticalCharacterRecognitionApp/Domain/Entity/Detector/DetectError.swift
+++ OpticalCharacterRecognitionApp/Domain/Entity/Detector/DetectError.swift
@@ -1,18 +1,5 @@
 import Foundation
 
-enum DetectError: Error, CustomDebugStringConvertible {
-    case imageNotFound
-    case filterNotFound
-    case detectFailed
-    
-    var debugDescription: String {
-        switch self {
-        case .imageNotFound:
-            "이미지를 찾을 수 없음"
-        case .filterNotFound:
-            "필터를 찾을 수 없음"
-        case .detectFailed:
-            "탐지 실패"
-        }
-    }
+enum DetectError: Error {
+    case rectangleDetectionFailed
 }
