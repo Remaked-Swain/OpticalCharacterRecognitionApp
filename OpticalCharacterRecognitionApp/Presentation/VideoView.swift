c7cda78fd01c5c8bd5e6e7f8a77466b0609f8c48
YunSwain <destap@naver.com>
2024-02-03 20:13:09 +0900

    refactor: 카메라 세션 Preview 구현

--- OpticalCharacterRecognitionApp/Presentation/VideoView.swift
+++ OpticalCharacterRecognitionApp/Presentation/VideoView.swift
@@ -1,7 +1,63 @@
 import UIKit
+import AVFoundation
 
-final class PreviewerView: UIView {
+final class VideoView: UIView {
+    // MARK: Properties
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
     
+    private lazy var videoPreviewLayer: AVCaptureVideoPreviewLayer = {
+        let layer = AVCaptureVideoPreviewLayer()
+        layer.frame = self.bounds
+        layer.videoGravity = .resizeAspect
+        self.layer.addSublayer(layer)
+        return layer
+    }()
     
+    var session: AVCaptureSession? {
+        get {
+            return videoPreviewLayer.session
+        }
+        set {
+            videoPreviewLayer.session = newValue
+        }
+    }
+    
+    private var highlightLayer: CAShapeLayer?
+    
+    var trackedRectangle: TrackedRectangle? {
+        didSet {
+            drawHighlightLayer()
+        }
+    }
+    
+    // MARK: Public
+    func removeHighlightLayer() {
+        highlightLayer?.removeFromSuperlayer()
+    }
+}
 
+// MARK: Draw Methods
+extension VideoView {
+    private func drawHighlightLayer() {
+        guard let trackedRectangle = trackedRectangle else { return }
+        print(trackedRectangle.boundingBox)
+        var transformedRect = trackedRectangle.boundingBox
+        transformedRect.origin.y = 1 - transformedRect.origin.y
+        let convertedRect = videoPreviewLayer.layerRectConverted(fromMetadataOutputRect: transformedRect)
+        
+        removeHighlightLayer()
+        
+        let layer = CAShapeLayer()
+        let path = UIBezierPath(rect: convertedRect)
+        layer.frame = bounds
+        layer.strokeColor = Theme.secondary(alpha: 1).color.cgColor
+        layer.fillColor = Theme.primary(alpha: 0.5).color.cgColor
+        layer.lineWidth = 4
+        layer.path = path.cgPath
+        
+        videoPreviewLayer.addSublayer(layer)
+        highlightLayer = layer
+    }
 }
