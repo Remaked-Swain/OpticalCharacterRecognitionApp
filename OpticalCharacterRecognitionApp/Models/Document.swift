41af297b5a99f020c933326620788180ad9fee0b
YunSwain <destap@naver.com>
2024-02-09 20:09:16 +0900

    refactor: Document 타입 Diffable 구현

--- OpticalCharacterRecognitionApp/Models/Document.swift
+++ OpticalCharacterRecognitionApp/Models/Document.swift
@@ -4,11 +4,29 @@ import CoreImage
 struct Document {
     let id: UUID
     let image: CIImage
-    let detectedRectangle: TrackedRectangle
+    let detectedRectangle: RectangleModel?
     
-    init(id: UUID = UUID(), image: CIImage, detectedRectangle: TrackedRectangle) {
+    init(id: UUID = UUID(), image: CIImage, detectedRectangle: RectangleModel? = nil) {
         self.id = id
         self.image = image
         self.detectedRectangle = detectedRectangle
+    }
+    
+    func changeDocument(newImage: CIImage? = nil, newDetectedRectangle: RectangleModel? = nil) -> Self {
+        let newDocument = Document(id: self.id,
+                                   image: newImage ?? self.image,
+                                   detectedRectangle: newDetectedRectangle ?? self.detectedRectangle)
+        return newDocument
+    }
+}
+
+// MARK: Hashable Confirmation
+extension Document: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func ==(lhs: Document, rhs: Document) -> Bool {
+        return lhs.id == rhs.id
     }
 }
