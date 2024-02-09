a3fdecf5dd85a6f3429836bb3cbb191c0d3671e4
YunSwain <destap@naver.com>
2024-02-09 20:10:14 +0900

    feat: 미리보기 화면 준비

--- OpticalCharacterRecognitionApp/ViewControllers/DocumentPagesViewController.swift
+++ OpticalCharacterRecognitionApp/ViewControllers/DocumentPagesViewController.swift
@@ -1,14 +1,86 @@
 import UIKit
 
 final class DocumentPagesViewController: UIViewController {
-    private let photoPersistentContainer: PersistentContainerProtocol
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Document>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Document>
     
-    init?(coder: NSCoder, photoPersistentContainer: PersistentContainerProtocol) {
-        self.photoPersistentContainer = photoPersistentContainer
+    // MARK: Dependencies
+    private let documentPersistentContainer: DocumentPersistentContainerProtocol
+    
+    // MARK: Properties
+    private var dataSource: DataSource?
+    private var snapshot: Snapshot = Snapshot()
+    
+    // MARK: IBOutlets
+    @IBOutlet private weak var documentCollectionView: UICollectionView!
+    
+    init?(coder: NSCoder, documentPersistentContainer: DocumentPersistentContainerProtocol) {
+        self.documentPersistentContainer = documentPersistentContainer
         super.init(coder: coder)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+//        changeNavigationTitle()
+    }
+    
+    // MARK: IBActions
+    @IBAction private func touchUpTrashButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction private func touchUpReversedClockButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction private func touchUpComposeButton(_ sender: UIBarButtonItem) {
+        
+    }
+}
+
+// MARK: Configure Methods
+extension DocumentPagesViewController {
+    private func configureDataSource() {
+        let cellRegistration = UICollectionView.CellRegistration<DocumentCell, Document> { cell, indexPath, itemIdentifier in
+            cell.configureCell(with: itemIdentifier)
+        }
+        
+        dataSource = DataSource(collectionView: documentCollectionView) { (documentCollectionView, indexPath, itemIdentifier) -> DocumentCell? in
+            let cell = documentCollectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+            return cell
+        }
+        
+        let allDocuments = documentPersistentContainer.resolve()
+        initializeDataSource(with: allDocuments)
+    }
+}
+
+// MARK: Private Methods
+extension DocumentPagesViewController {
+    private func changeNavigationTitle(_ index: Int) {
+        // TODO: NavigationTitle 바뀌어야됨 스크롤 될떄마다
+        navigationItem.title = "\(index + 1)/\(documentPersistentContainer.count)"
+    }
+}
+
+// MARK: DataSource Controls
+extension DocumentPagesViewController {
+    private enum Section: Int, CaseIterable {
+        case main = 0
+        
+        var sectionNumber: Int {
+            self.rawValue
+        }
+    }
+    
+    private func initializeDataSource(with documents: [Document]) {
+        snapshot.appendSections(Section.allCases)
+        snapshot.appendItems(documents)
+        dataSource?.apply(snapshot, animatingDifferences: true)
     }
 }
--- OpticalCharacterRecognitionApp/Views/DocumentCell.swift
+++ OpticalCharacterRecognitionApp/Views/DocumentCell.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 final class DocumentCell: UICollectionViewCell {
+    // MARK: Properties
+    private var document: Document?
     
+    // MARK: IBOutlets
+    @IBOutlet private weak var documentImageView: UIImageView!
+    
+    
+}
+
+// MARK: Configure Methods
+extension DocumentCell {
+    func configureCell(with document: Document) {
+        self.document = document
+    }
 }
