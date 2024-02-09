8e3c4a3156c46d7b38af58efa4fad8c2276c10d1
YunSwain <destap@naver.com>
2024-02-09 20:08:13 +0900

    refactor: Detector 내 로직 중 일부 변수명 변경

--- OpticalCharacterRecognitionApp/Models/Detector/DefaultDocumentDetector.swift
+++ OpticalCharacterRecognitionApp/Models/Detector/DefaultDocumentDetector.swift
@@ -2,7 +2,7 @@ import Foundation
 import CoreImage
 
 protocol DocumentDetector: AnyObject {
-    func detect(in ciImage: CIImage) -> TrackedRectangle?
+    func detect(in ciImage: CIImage) throws -> RectangleModel
 }
 
 final class DefaultDocumentDetector: DocumentDetector {
@@ -10,17 +10,18 @@ final class DefaultDocumentDetector: DocumentDetector {
     private let detector: CIDetector? = CIDetector(ofType: CIDetectorTypeRectangle,
                                                    context: nil)
     private let options: [String: Any] = [
-        CIDetectorAccuracy: CIDetectorAccuracyHigh
+        CIDetectorAccuracy: CIDetectorAccuracyHigh,
+        CIDetectorAspectRatio: 1.75
     ]
     
     // MARK: Interface
-    func detect(in ciImage: CIImage) -> TrackedRectangle? {
+    func detect(in ciImage: CIImage) throws -> RectangleModel {
         guard let rectangles = detector?.features(in: ciImage, options: options) as? [CIRectangleFeature],
               let rectangle = rectangles.first
         else {
-            return nil
+            throw DetectError.rectangleDetectionFailed
         }
-        let trackedRectangle = TrackedRectangle(rectangleFeature: rectangle)
-        return trackedRectangle
+        let detectedRectangle = RectangleModel(rectangleFeature: rectangle)
+        return detectedRectangle
     }
 }
--- OpticalCharacterRecognitionApp/Models/Detector/DetectError.swift
+++ OpticalCharacterRecognitionApp/Models/Detector/DetectError.swift
@@ -1,5 +1,12 @@
 import Foundation
 
-enum DetectError: Error {
+enum DetectError: Error, CustomDebugStringConvertible {
     case rectangleDetectionFailed
+    
+    var debugDescription: String {
+        switch self {
+        case .rectangleDetectionFailed:
+            "사각형 감지 실패"
+        }
+    }
 }
