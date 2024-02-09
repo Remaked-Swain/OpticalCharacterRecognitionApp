82ccca2e205cef0b23276772ac5c128193ddb5b4
YunSwain <destap@naver.com>
2024-02-09 20:08:28 +0900

    feat: 필터링 기능 추가

--- OpticalCharacterRecognitionApp/Models/Filter/PerspectiveCorrectionFilter.swift
+++ OpticalCharacterRecognitionApp/Models/Filter/PerspectiveCorrectionFilter.swift
@@ -1,8 +1,20 @@
-//
-//  PerspectiveCorrectionFilter.swift
-//  OpticalCharacterRecognitionApp
-//
-//  Created by Swain Yun on 2/8/24.
-//
-
+import CoreImage
 import Foundation
+
+protocol FilterProtocol {
+    func filter(filterType type: FilterType, image: CIImage, rectangle: RectangleModel) -> CIImage
+}
+
+struct Filter: FilterProtocol {
+    func filter(filterType type: FilterType, image: CIImage, rectangle: RectangleModel) -> CIImage {
+        let filteredImage = image.applyingFilter(type.name,
+                                                 parameters: [
+                                                    "inputTopLeft": CIVector(cgPoint: rectangle.topLeft),
+                                                    "inputTopRight": CIVector(cgPoint: rectangle.topRight),
+                                                    "inputBottomLeft": CIVector(cgPoint: rectangle.bottomLeft),
+                                                    "inputBottomRight": CIVector(cgPoint: rectangle.bottomRight),
+                                                 ])
+        
+        return filteredImage
+    }
+}
