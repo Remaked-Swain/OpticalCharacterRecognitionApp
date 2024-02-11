d1bfb8937917decf6c53c0ae78c092c9fbdf0d92
YunSwain <destap@naver.com>
2024-02-12 01:09:23 +0900

    chore: DocumentDetector의 필터링 기능 수행을 위한 폴더링

---
+++ OpticalCharacterRecognitionApp/Models/Detector/Filter/FilterProtocol.swift
@@ -0,0 +1,19 @@
+import CoreImage
+import Foundation
+
+protocol FilterProtocol {
+    func filter(filterType type: FilterType, image: CIImage, rectangle: RectangleModel) -> CIImage
+}
+
+extension FilterProtocol {
+    func filter(filterType type: FilterType, image: CIImage, rectangle: RectangleModel) -> CIImage {
+        let filteredImage = image.applyingFilter(type.name,
+                                                 parameters: [
+                                                    "inputTopLeft": CIVector(cgPoint: rectangle.topLeft),
+                                                    "inputTopRight": CIVector(cgPoint: rectangle.topRight),
+                                                    "inputBottomLeft": CIVector(cgPoint: rectangle.bottomLeft),
+                                                    "inputBottomRight": CIVector(cgPoint: rectangle.bottomRight),
+                                                 ])
+        return filteredImage
+    }
+}
