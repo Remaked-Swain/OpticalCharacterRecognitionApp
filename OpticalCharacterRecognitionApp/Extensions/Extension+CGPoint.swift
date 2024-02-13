c8b7ce27486cc4adc8998065147e999a5c59e118
YunSwain <destap@naver.com>
2024-02-13 21:37:23 +0900

    chore: 단순 코드 변경 및 주석 추가

--- OpticalCharacterRecognitionApp/Extensions/Extension+CGPoint.swift
+++ OpticalCharacterRecognitionApp/Extensions/Extension+CGPoint.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 extension CGPoint {
+    /**
+     목표한 높이만큼의 규모 내에서 Y축을 반전시킵니다.
+     
+     목표한 높이만큼의 스케일링 후 X좌표와 Y좌표가 서로 바뀝니다.
+     */
     func flipPositionY(_ scaleX: Double, _ scaleY: Double, displayTargetHeight height: CGFloat) -> Self {
         return Self(x: self.x * scaleX, y: height - (self.y * scaleY))
     }
@@ -11,6 +16,7 @@ extension CGPoint {
     func distance(to point: Self) -> CGFloat {
         let dx = self.x - point.x
         let dy = self.y - point.y
-        return sqrt(dx * dx + dy * dy)
+        let distance = sqrt(dx * dx + dy * dy)
+        return distance
     }
 }
