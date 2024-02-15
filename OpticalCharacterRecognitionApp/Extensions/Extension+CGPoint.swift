bcc5fa623d1b6304c8078a54d771b475f588c057
YunSwain <destap@naver.com>
2024-02-15 17:43:59 +0900

    chore: 불필요 코드 제거

--- OpticalCharacterRecognitionApp/Extensions/Extension+CGPoint.swift
+++ OpticalCharacterRecognitionApp/Extensions/Extension+CGPoint.swift
@@ -9,14 +9,4 @@ extension CGPoint {
     func flipPositionY(_ scaleX: Double, _ scaleY: Double, displayTargetHeight height: CGFloat) -> Self {
         return Self(x: self.x * scaleX, y: height - (self.y * scaleY))
     }
-    
-    /**
-     두 점의 좌표 차이를 구합니다.
-     */
-    func distance(to point: Self) -> CGFloat {
-        let dx = self.x - point.x
-        let dy = self.y - point.y
-        let distance = sqrt(dx * dx + dy * dy)
-        return distance
-    }
 }
